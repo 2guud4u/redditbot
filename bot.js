15fad89ca20df46113064d0bdf1278d45fc1c9d8
Jiahua Jiang <jiahua212@gmail.com>
2023-01-25 15:45:05 -0800

    bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,19 @@ r = new snoowrap({
     password: 'Happyguy20031024'
 });
 
+class post{
+    constructor(url, rating, wtc){
+        this.url = url;
+        this.rating = rating;
+        this.wtc = wtc;
+    }
+    link(){
+        console.log(url);
+    }
+}
 r.search({
-    query: 'unc',
+    query: 'slides',
     subreddit: 'FashionReps',
-    sort: 'comments'}
-).then(console.log)
+    sort: 'relevance'}
+).filter(post=>post.link_flair_text == "QC").map(post=>post.permalink).then(console.log)
 
